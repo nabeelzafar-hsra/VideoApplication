364715211803f338c62c9ee5fb9df386a1c3c04f
Nabeel Zafar <nibi.zafar@hotmail.com>
2016-06-06 14:42:34 +0500

    Initial Commit

---
+++ VideoApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoApplication
+//
+//  Created by Zafar Saleem on 06/06/2016.
+//  Copyright © 2016 Zafar Saleem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
